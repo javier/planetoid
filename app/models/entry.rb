cf13577d3cd2a383cff4893719e3986dff63cba8
javier <javier.ramirez.gomara@gmail.com>
2009-10-06 01:47:40 +0200

    twitter gem version

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -5,11 +5,16 @@ class Entry < ActiveRecord::Base
   validates_uniqueness_of :url
   
   def after_create
+    twit_me
+    return nil
+  end
+  
+  def twit_me
     begin
       if PLANETOID_CONF[:twitter][:send_updates] && self.published > self.feed.created_at
-      httpauth = Twitter::HTTPAuth.new PLANETOID_CONF[:twitter][:user],PLANETOID_CONF[:twitter][:password]
-      twit=Twitter::Base.new(httpauth)
-      twit.update "#{PLANETOID_CONF[:twitter][:update_prefix]} #{self.title[0..150]} #{self.url}"
+        httpauth = Twitter::HTTPAuth.new PLANETOID_CONF[:twitter][:user],PLANETOID_CONF[:twitter][:password]
+        twit=Twitter::Base.new(httpauth)
+        twit.update "#{PLANETOID_CONF[:twitter][:update_prefix]} #{self.title[0..150]} #{self.url}"
       end
     rescue Exception => e
       puts e.message
